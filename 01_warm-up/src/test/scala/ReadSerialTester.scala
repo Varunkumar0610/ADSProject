d73a0eb651beeac8a589182aa8973ef1fa35bc97
Varun Kumar <varunkumar@Varuns-MacBook-Air.local>
2026-05-29 17:08:33 +0200

    updated Project files

--- 01_warm-up/src/test/scala/ReadSerialTester.scala
+++ 01_warm-up/src/test/scala/ReadSerialTester.scala
@@ -1,17 +1,9 @@
-// ADS I Class Project
-// Chisel Introduction
-//
-// Chair of Electronic Design Automation, RPTU in Kaiserslautern
-
 package readserial
 
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
-/** 
-  * Read Serial Tester
-  */
 class ReadSerialTester extends AnyFlatSpec with ChiselScalatestTester {
 
   "ReadSerial" should "work" in {
@@ -19,10 +11,12 @@ class ReadSerialTester extends AnyFlatSpec with ChiselScalatestTester {
     test(new ReadSerial)
       .withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
 
-      
-      // Initial idle condition
-      // Bus idle = 1
-      
+      // Release reset
+
+      dut.io.reset_n.poke(false.B)
+
+      // Idle bus
+
       dut.io.rxd.poke(true.B)
 
       dut.clock.step(2)
@@ -31,108 +25,88 @@ class ReadSerialTester extends AnyFlatSpec with ChiselScalatestTester {
 
       
       // TEST CASE 1
-      // Receive: 10101010
-      // MSB first
+      // 10101010
       
 
-      // Start bit
       dut.io.rxd.poke(false.B)
       dut.clock.step(1)
 
-      // Data bits
-      dut.io.rxd.poke(true.B)   // bit7
-      dut.clock.step(1)
+      val data1 = Seq(
+        true.B,
+        false.B,
+        true.B,
+        false.B,
+        true.B,
+        false.B,
+        true.B,
+        false.B
+      )
 
-      dut.io.rxd.poke(false.B)  // bit6
-      dut.clock.step(1)
+      for(bit <- data1){
 
-      dut.io.rxd.poke(true.B)   // bit5
-      dut.clock.step(1)
+        dut.io.rxd.poke(bit)
 
-      dut.io.rxd.poke(false.B)  // bit4
-      dut.clock.step(1)
-
-      dut.io.rxd.poke(true.B)   // bit3
-      dut.clock.step(1)
-
-      dut.io.rxd.poke(false.B)  // bit2
-      dut.clock.step(1)
-
-      dut.io.rxd.poke(true.B)   // bit1
-      dut.clock.step(1)
-
-      dut.io.rxd.poke(false.B)  // bit0
-      dut.clock.step(1)
-
-      // Check output
-      //dut.clock.step(1)
+        dut.clock.step(1)
+      }
 
       dut.io.valid.expect(true.B)
 
       dut.io.data.expect("b10101010".U)
 
+      dut.clock.step(1)
+
       
       // TEST CASE 2
-      // Receive: 11111111
-      // All ones
+      // RESET DURING TRANSMISSION
       
 
-      // Start bit
+      dut.io.rxd.poke(false.B)   // start bit
+      dut.clock.step(1)
+
+      dut.io.rxd.poke(true.B)    // bit7
+      dut.clock.step(1)
+
+      dut.io.rxd.poke(false.B)   // bit6
+      dut.clock.step(1)
+
+      dut.io.rxd.poke(true.B)    // bit5
+      dut.clock.step(1)
+
+      
+
+      dut.io.reset_n.poke(true.B)
+
+      dut.clock.step(1)
+
+      // Receiver should be reset
+
+      dut.io.valid.expect(false.B)
+
+      //dut.io.data.expect(0.U)
+
+      // Release reset
+
+      dut.io.reset_n.poke(false.B)
+
+      dut.clock.step(1)
+
+      // Start a NEW transmission
+
       dut.io.rxd.poke(false.B)
       dut.clock.step(1)
 
-      for(i <- 0 until 8) {
-
-        dut.io.rxd.poke(true.B)
-
-        dut.clock.step(1)
-      }
-
-      dut.io.valid.expect(true.B)
-
-      dut.io.data.expect("b11111111".U)
-
-
-      
-      // TEST CASE 3
-      // Receive: 00000000
-      // All zeros
-      
-
-      // Start bit
-      dut.io.rxd.poke(false.B)
-      dut.clock.step(1)
-
-      for(i <- 0 until 8) {
-
-        dut.io.rxd.poke(false.B)
-
-        dut.clock.step(1)
-      }
-
-      dut.io.valid.expect(true.B)
-
-      dut.io.data.expect("b00000000".U)
-
-      
-      // TEST CASE 4
-      // Back-to-back transmission
-      // No idle cycle in between
-      
-
-      // Start bit of first transmission
-      dut.io.rxd.poke(false.B)
-      dut.clock.step(1)
-
-      // 11001100
-      val data1 = Seq(
-        true.B, true.B,
-        false.B, false.B,
-        true.B, true.B,
-        false.B, false.B
+      val resetData = Seq(
+        true.B,
+        true.B,
+        true.B,
+        true.B,
+        false.B,
+        false.B,
+        false.B,
+        false.B
       )
 
-      for(bit <- data1) {
+      for(bit <- resetData){
 
         dut.io.rxd.poke(bit)
 
@@ -141,36 +115,10 @@ class ReadSerialTester extends AnyFlatSpec with ChiselScalatestTester {
 
       dut.io.valid.expect(true.B)
 
-      dut.io.data.expect("b11001100".U)
-
-      // Immediately next transmission
-      // Start bit again
-      dut.io.rxd.poke(false.B)
-      dut.clock.step(1)
-
-      // 00110011
-      val data2 = Seq(
-        false.B, false.B,
-        true.B, true.B,
-        false.B, false.B,
-        true.B, true.B
-      )
-
-      for(bit <- data2) {
-
-        dut.io.rxd.poke(bit)
-
-        dut.clock.step(1)
-      }
-
-      dut.io.valid.expect(true.B)
-
-      dut.io.data.expect("b00110011".U)
+      dut.io.data.expect("b11110000".U)
 
       dut.clock.step(5)
 
-
-      
-    }
+      }
   }
 }
